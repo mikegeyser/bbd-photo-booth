2a04473390d542147d76e4d42fd404e16000d677
Mike Geyser <mikegeyser@gmail.com>
2017-09-26 16:20:47 +0200

    First draft finished.

--- web/index.js
+++ web/index.js
@@ -1,6 +1,10 @@
 let recording = false;
 let gifShown = false;
-let gifElement, videoElement, loadingElement;
+let gifElement,
+  videoElement,
+  loadingElement,
+  recordingMessageElement,
+  completedMessageElement;
 
 function clickButton() {
   if (gifShown) {
@@ -8,6 +12,9 @@ function clickButton() {
     gifElement.src = "";
     gifElement.style.display = "none";
     videoElement.style.display = "block";
+    initialMessageElement.style.display = "block";
+    recordingMessageElement.style.display = "none";
+    completedMessageElement.style.display = "none";
   } else {
     startCountdown();
   }
@@ -17,6 +24,8 @@ function startCountdown() {
   if (!recording) {
     recording = true;
     let timer = 3;
+
+    initialMessageElement.style.display = "none";
 
     let overlay = document.getElementById("overlay");
     overlay.style.display = "block";
@@ -34,7 +43,6 @@ function startCountdown() {
   }
 }
 
-
 let video = document.getElementById("video");
 let stream = null;
 
@@ -46,24 +54,24 @@ let resolution = {
 
 let mediaRecorder;
 function record() {
-  console.log("recording...")
+  console.log("recording...");
   const options = { mimeType: "video/webm" };
   const recordedChunks = [];
   mediaRecorder = new MediaRecorder(stream, options);
 
-  mediaRecorder.addEventListener("dataavailable", function (e) {
+  mediaRecorder.addEventListener("dataavailable", function(e) {
     if (e.data.size > 0) {
       recordedChunks.push(e.data);
     }
   });
 
-  mediaRecorder.addEventListener("stop", function () {
+  mediaRecorder.addEventListener("stop", function() {
     loadingElement.style.display = "block";
     let downloadLink = document.getElementById("download");
     let blob = new Blob(recordedChunks);
 
     var formdata = new FormData();
-    formdata.append('video', blob);
+    formdata.append("video", blob);
 
     var xhr = new XMLHttpRequest();
 
@@ -78,9 +86,9 @@ function record() {
         videoElement.style.display = "none";
         gifElement.src = xhr.responseText;
         gifShown = true;
+        document.getElementById("completed-message").style.display = "block";
       }
     };
-
   });
 
   mediaRecorder.start();
@@ -89,39 +97,42 @@ function record() {
 }
 
 function stop() {
-
-  let recordDiv = document.getElementById("recording");
-  recordDiv.style.display = "block";
+  recordingMessageElement.style.display = "block";
   setTimeout(() => {
     mediaRecorder.stop();
-    recordDiv.style.display = "none";
+    recordingMessageElement.style.display = "none";
     recording = false;
   }, 3000);
 }
 
 function startCamera() {
-  loadingElement = document.getElementById("load")
+  loadingElement = document.getElementById("load");
   gifElement = document.getElementById("gif");
   videoElement = document.getElementById("video");
+  initialMessageElement = document.getElementById("initial-message");
+  recordingMessageElement = document.getElementById("recording-message");
+  completedMessageElement = document.getElementById("completed-message");
 
   loadingElement.style.display = "none";
   gifElement.style.display = "none";
+  completedMessageElement.style.display = "none";
+  initialMessageElement.style.display = "block";
   videoElement.style.display = "block";
 
   navigator.mediaDevices
     .getUserMedia({ video: resolution, audio: false })
-    .then(function (s) {
+    .then(function(s) {
       stream = s;
       video.srcObject = s;
       video.play();
     })
-    .catch(function (err) {
+    .catch(function(err) {
       console.log("An error occured! " + err);
     });
 
   video.addEventListener(
     "canplay",
-    function (ev) {
+    function(ev) {
       height = video.videoHeight;
       width = video.videoWidth;
       video.setAttribute("width", width);
@@ -131,5 +142,11 @@ function startCamera() {
     false
   );
 }
+
+window.addEventListener("keydown", event => {
+  if (event.key === " " || event.key === "Space Bar") {
+    clickButton();
+  }
+});
 
 startCamera();
